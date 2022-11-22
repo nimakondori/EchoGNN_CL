0cd6c26a5ae09828b5d3cd07b8feeda6e2b0805b
nimakondori <nimakondori96@gmail.com>
2022-11-22 10:54:57 -0800

    added random state and fixed the pretrain path

--- src/core/datasets.py
+++ src/core/datasets.py
@@ -80,8 +80,9 @@ class EchoNetEfDataset(Dataset, ABC):
         # CSV file containing file names and labels
         filelist_df = pd.read_csv(os.path.join(dataset_path, 'FileList.csv'))
 
-        if sample_size != 1:
-            filelist_df = filelist_df.sample(n=int(sample_size * len(filelist_df)))
+        if sample_size != 1.0:
+            # Random state set for reproducibility of the data
+            filelist_df = filelist_df.sample(n=int(sample_size * len(filelist_df)), random_state=42)
 
         # Extract Split information
         splits = np.array(filelist_df['Split'].tolist())
